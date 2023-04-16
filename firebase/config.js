166db2edf63c3b99299c711a6080185fb9ec1af7
Roberto Núñez <robertonc1599@gmail.com>
2023-04-16 10:03:53 -0500

    Producthunt project has been finished

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBfcb4rvaaZ_kN2Jl1CA844G_aI8BEIWdY",
+    authDomain: "product-hunt-43c92.firebaseapp.com",
+    projectId: "product-hunt-43c92",
+    storageBucket: "product-hunt-43c92.appspot.com",
+    messagingSenderId: "1028379002179",
+    appId: "1:1028379002179:web:fec24d02c72075db5b5bf6"
+};
+
+export default firebaseConfig;
